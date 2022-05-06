98899799cdb6f69915263a6057ab29d497fa3c16
Nicolle1998 <nicolle.almeidadesouza@gmail.com>
2022-05-06 13:26:14 -0300

    commit atualizado

--- src/Dados/dadosJogos.js
+++ src/Dados/dadosJogos.js
@@ -1,38 +1,34 @@
-const JOGOS =  [
+const JOGOS = [
     {
-        id : "01",
-        nome : "Subway Surfers",
-        valor : 13.32,
-        imagem: 'Subway.jpg'
-
+        id: '01',
+        nome: 'Subway Surfers',
+        valor: 13.32,
+        imagem: 'subway.jpg',
     },
     {
-        id : "02",
-        nome : "Yugioh Master Duel",
-        valor : "gratuito",
-        imagem: "yugi.jpg"
-
+        id: '02',
+        nome: 'Yugioh Master Duel',
+        valor: 'gratuito',
+        imagem: 'yugi.jpg',
     },
     {
-        id : "03",
-        nome : "Pokemon go",
-        valor : "gratuito",
-        imagem: 'pokemon.jpg'
-
+        id: '03',
+        nome: 'Pokemon Go',
+        valor: 'gratuito',
+        imagem: 'pokemon.jpg',
     },
     {
-        id : "04",
-        nome : "Hora da aventura",
-        valor : "R$10,00",
-        imagem: 'aventura.jpg'
-
+        id: '04',
+        nome: 'Hora de Aventura',
+        valor: 'R$10,00',
+        imagem: 'aventura.jpg',
     },
     {
-        id : "05",
-        nome : 'Dragon Ball Fight',
-        valor : 'R$18,00',
-        imagem: 'dbz.jpg'
-
-    }
+        id: '05',
+        nome: 'Dragon Ball Fight',
+        valor: 'R$18,00',
+        imagem: 'dbz.jpg',
+    },
 ]
+
 export default JOGOS;
--- src/components/botao/index.js
+++ src/components/botao/index.js
@@ -1,55 +1,46 @@
-import React, { fragment } from "react";
-import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React,{Fragment} from 'react';
+import {View,Image,Text,TouchableOpacity,StyleSheet} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
-export default function Botao({ Logo, texto, cor, Logo2, texto2, cor2 }) {
-  return (
-    <View style={estilo.containerBotoes1}>
-      <TouchableOpacity style={[estilo.botoes, { backgroundColor: cor }]}>
-        <Ionicons
-          name={Logo}
-          size={20}
-          color="black"
-          style={{ marginRight: 10 }}
-        />
-        <Text style={estilo.textoBotao}> {texto} </Text>
-      </TouchableOpacity>
+export default function Botao({logo,texto,cor,logo2,texto2,cor2}){
 
-      <TouchableOpacity style={[estilo.botoes, { backgroundColor: cor2 }]}>
-        <Ionicons
-          name={Logo2}
-          size={20}
-          color="black"
-          style={{ marginRight: 10 }}   
-        />
-        <Text style={estilo.textBotao}> {texto2} </Text>
-        </TouchableOpacity>
-      
-    </View>
-  );
+    return(
+        <View style={estilo.containerBotoes1}>
+            <TouchableOpacity style={[estilo.botoes ,{backgroundColor: cor}]}>
+                <Ionicons name={logo} size={20} color="black" style={{marginRight:10}}/>
+                <Text style={estilo.textoBotao} > {texto} </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[estilo.botoes ,{backgroundColor: cor2}]}>
+                <Ionicons name={logo2} size={20} color="black" style={{marginRight:10}}/>
+                <Text style={estilo.textoBotao} > {texto2} </Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const estilo = StyleSheet.create({
-  containerBotoes1: {
-    flexDirection: "row",
-    marginBottom: 5,
-    justifyContent: "center",
-   
-  },
-  textBotao: {
-    color: "black",
-    fontweight: "bold",
-  },
-  botoes: {
-    borderWidth: 1,
-    borderColor: "black",
-    padding: 5,
-    marginHorizontal: 10,
-    width: "45%",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    fontSize: 12,
-    color: "white",
-    flexDirection: "row",
-  },
-});
+
+    containerBotoes1:{
+        flexDirection:'row',
+        marginBottom:10,
+        justifyContent:"center"
+    },
+
+    textoBotao:{
+        color:'white',
+        fontWeight:'bold'
+    },
+
+    botoes:{
+        padding:5,
+        marginHorizontal:10,
+        width:'45%',
+        alignItems:'center',
+        justifyContent:'flex-start',
+        borderRadius: 5,
+        
+        fontSize:12,
+        color:'#00BFFF',
+        flexDirection:'row',
+    }
+})
